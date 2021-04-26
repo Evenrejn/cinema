7fa64064b8a91b71113dd83e6efe0cb72bfdbeaf
Evenrejn <brendivajn@gmail.com>
2021-04-26 11:21:18 +0300

    bug fixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
+
+const adv = document.querySelectorAll(".promo__adv img"),
+      poster = document.querySelector(".promo__bg"),
+      genre = poster.querySelector(".promo__genre"),
+      movieList = document.querySelector(".promo__interactive-list");
 
 const movieDB = {
     movies: [
@@ -24,14 +15,25 @@ const movieDB = {
     ]
 };
 
-movieDB.movies.sort((a, b) => a.localeCompare(b));
+adv.forEach(item => {
+    item.remove();
+});
 
-document.querySelector(".promo__adv").remove();
-document.querySelector(".promo__genre").innerText = "Драма";
-document.querySelector(".promo__bg").style.background = "url(img/bg.jpg) center center/cover no-repeat";
+genre.textContent = "Драма";
 
-for (let i = 1; i <= movieDB.movies.length; i++) {
-    document.querySelector(".promo__interactive-title").insertAdjacentHTML("beforeEnd", `<br><li class="promo__interactive-item">${i} ${movieDB.movies[i-1]}<div class="delete"></div></li>`);
-}
+poster.style.background = "url('img/bg.jpg') center center/cover no-repeat";
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
 
 
