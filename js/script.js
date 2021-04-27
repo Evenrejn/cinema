60c6ab78651228fad19e3887b49167c85cbf868e
Evenrejn <brendivajn@gmail.com>
2021-04-27 15:06:22 +0300

    add dynamic list

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", () => {
+    const adv = document.querySelectorAll(".promo__adv img"),
+    poster = document.querySelector(".promo__bg"),
+    genre = poster.querySelector(".promo__genre"),
+    movieList = document.querySelector(".promo__interactive-list"),
+    addForm = document.querySelector("form.add"),
+    addInput = addForm.querySelector(".adding__input"),
+    checkbox = addForm.querySelector("[type=checkbox]");
 
-const adv = document.querySelectorAll(".promo__adv img"),
-      poster = document.querySelector(".promo__bg"),
-      genre = poster.querySelector(".promo__genre"),
-      movieList = document.querySelector(".promo__interactive-list");
-
-const movieDB = {
+    const movieDB = {
     movies: [
         "Одержимость",
         "Логан",
@@ -13,27 +16,66 @@ const movieDB = {
         "Ла-ла лэнд",
         "Скотт Пилигрим против..."
     ]
-};
+    };
 
-adv.forEach(item => {
-    item.remove();
+    adv.forEach(item => {
+        item.remove();
+    });
+
+    genre.textContent = "Драма";
+
+    poster.style.background = "url('img/bg.jpg') center center/cover no-repeat";
+
+    movieList.innerHTML = "";
+
+
+    const newFilm = document.querySelector(".adding__input");
+
+    const confirmBtn = document.querySelector("button");
+
+    updateFilmList();
+
+    const likeFilm = document.querySelector("input[type='checkbox']");
+
+    confirmBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (newFilm.value.length != 0 && newFilm.value.length < 21) {
+        movieDB.movies.push(newFilm.value.toUpperCase());
+        addToLikedFilm();
+    } else if (newFilm.value.length > 21) {
+        movieDB.movies.push(newFilm.value.slice(0, 21).toUpperCase()+"...");
+        addToLikedFilm();
+    }
+
+    newFilm.value = "";
+    updateFilmList();
+    });
+
+    function updateFilmList() {
+    movieList.innerHTML = "";
+    movieDB.movies.sort();
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+    }
+
+    function addToLikedFilm() {
+    if (likeFilm.checked) {
+        console.log("Добавлено в любимые фильмы");
+        likeFilm.checked = false;
+    }
+    }
+
+    let delFilm = document.querySelectorAll(".delete");
+    delFilm.forEach(element => {
+    element.addEventListener("click", function(event) {
+        console.log(element.previousSibling);
+    });
+    });
 });
-
-genre.textContent = "Драма";
-
-poster.style.background = "url('img/bg.jpg') center center/cover no-repeat";
-
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
 
 
